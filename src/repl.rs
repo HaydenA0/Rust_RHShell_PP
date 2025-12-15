a2253ade6eb70407e7da064de106209b6e6ec8fe
HaydenA0 <aregrag2005@gmail.com>
2025-12-15 20:52:26 +0100

    something

--- src/repl.rs
+++ src/repl.rs
@@ -7,7 +7,7 @@ use crate::{
 
 pub fn repl_loop() {
     loop {
-        eprint!("RH\n$ ");
+        eprint!("RH\n $ ");
         let mut user_input = String::new();
         io::stdin()
             .read_line(&mut user_input)
